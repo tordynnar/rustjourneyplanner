a186b4abca087a78519400ed7338de89e492bd24
tordynnar <tordynnar@protonmail.com>
2023-12-02 00:59:36 +1100

    Changes

--- dist/rustjourneyplanner-108b9b5739a1f5cb.js
+++ dist/rustjourneyplanner-108b9b5739a1f5cb.js
@@ -714,7 +714,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('rustjourneyplanner-bba74e34362fd1fe_bg.wasm', import.meta.url);
+        input = new URL('rustjourneyplanner-108b9b5739a1f5cb_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
