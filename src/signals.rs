1764d86fe44284fbb136d88ba79a864a524da051
tordynnar <tordynnar@protonmail.com>
2023-12-16 03:23:27 +1100

    Changes

--- src/signals.rs
+++ src/signals.rs
@@ -7,7 +7,7 @@ pub fn create_local_resource_timed<T, Fu>(
     fetcher: impl Fn() -> Fu + 'static + Copy
 ) -> Resource<u64, T>
 where
-    T: 'static,
+    T: 'static,   // implement Clone here
     Fu: Future<Output = T> + 'static,
 {
     let (refresh, set_refresh) = create_signal(0u64);
@@ -20,6 +20,8 @@ where
         //let zz = x.get_value();
         //info!("{}", zz);
         //x.set_value(zz + 1);
+
+        // Clone previous value
 
         set_timeout(move || { set_refresh.update(|v| *v += 1); }, std::time::Duration::from_millis(millis));
         result
