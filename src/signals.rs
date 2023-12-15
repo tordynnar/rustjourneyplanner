a3b153edfbf41b8ba3e5e0c5ba6df0baa718e2e2
tordynnar <tordynnar@protonmail.com>
2023-12-16 03:22:15 +1100

    ChangeS

--- src/signals.rs
+++ src/signals.rs
@@ -1,5 +1,6 @@
 use leptos::*;
 use std::future::Future;
+use tracing::info;
 
 pub fn create_local_resource_timed<T, Fu>(
     millis : u64,
@@ -11,8 +12,15 @@ where
 {
     let (refresh, set_refresh) = create_signal(0u64);
 
+    //let x = store_value(0u32);
+
     create_local_resource(move || { refresh.get() }, move |_| async move {
         let result = fetcher().await;
+
+        //let zz = x.get_value();
+        //info!("{}", zz);
+        //x.set_value(zz + 1);
+
         set_timeout(move || { set_refresh.update(|v| *v += 1); }, std::time::Duration::from_millis(millis));
         result
     })
